1d03bb6bfea1252eaa53c38225bc38032d5081c1
GroupInfinity21 <grupoviverinfinity@gmail.com>
2025-04-07 17:24:13 -0300

    Adiciona Meta Pixel ao layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import './globals.css';
 import { Inter } from 'next/font/google';
 
@@ -23,7 +24,23 @@ export default function RootLayout({
         />
         <link rel="icon" href="/favicon-16x16.png" type="image/x-icon" />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+
+      <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1065589662062053');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
